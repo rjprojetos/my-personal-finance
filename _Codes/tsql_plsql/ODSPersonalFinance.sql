09b68480f57f6dcd318dcdd875a36513d8a05b35
leonardo <leonardo@ictn.com.br>
2013-07-24 04:33:28 +0000

    Pacotes das Tmps completos

--- _Codes/tsql_plsql/ODSPersonalFinance.sql
+++ _Codes/tsql_plsql/ODSPersonalFinance.sql
@@ -1,7 +1,7 @@
 /*==============================================================*/
 /* Database name:  ODS_PERSONAL_FINANCE                         */
 /* DBMS name:      Microsoft SQL Server 2008                    */
-/* Created on:     22/07/2013 17:15:31                          */
+/* Created on:     23/07/2013 07:26:37                          */
 /*==============================================================*/
 
 
@@ -24,9 +24,38 @@ go
 
 IF EXISTS (SELECT 1
             FROM  SYSOBJECTS
+           WHERE  ID = OBJECT_ID('LOG_CAT_SUBCAT_NO_MATCH')
+            AND   TYPE = 'U')
+   DROP TABLE LOG_CAT_SUBCAT_NO_MATCH
+go
+
+IF EXISTS (SELECT 1
+            FROM  SYSOBJECTS
+           WHERE  ID = OBJECT_ID('LOG_CONTA_NO_MATCH')
+            AND   TYPE = 'U')
+   DROP TABLE LOG_CONTA_NO_MATCH
+go
+
+IF EXISTS (SELECT 1
+            FROM  SYSOBJECTS
+           WHERE  ID = OBJECT_ID('LOG_TRANSACAO_NO_MATCH')
+            AND   TYPE = 'U')
+   DROP TABLE LOG_TRANSACAO_NO_MATCH
+go
+
+ALTER TABLE TB_CONTA
+   DROP CONSTRAINT PK_TB_CONTA
+go
+
+IF EXISTS (SELECT 1
+            FROM  SYSOBJECTS
            WHERE  ID = OBJECT_ID('TB_CONTA')
             AND   TYPE = 'U')
    DROP TABLE TB_CONTA
+go
+
+ALTER TABLE TB_TRANSACAO
+   DROP CONSTRAINT PK_TB_TRANSACAO
 go
 
 IF EXISTS (SELECT 1
@@ -34,6 +63,10 @@ IF EXISTS (SELECT 1
            WHERE  ID = OBJECT_ID('TB_TRANSACAO')
             AND   TYPE = 'U')
    DROP TABLE TB_TRANSACAO
+go
+
+ALTER TABLE TMP_CATEGORIA
+   DROP CONSTRAINT PK_TMP_CATEGORIA
 go
 
 IF EXISTS (SELECT 1
@@ -50,6 +83,10 @@ IF EXISTS (SELECT 1
    DROP TABLE TMP_FT_LANCAMENTOS
 go
 
+ALTER TABLE TMP_SUB_CATEGORIA
+   DROP CONSTRAINT PK_TMP_SUB_CATEGORIA
+go
+
 IF EXISTS (SELECT 1
             FROM  SYSOBJECTS
            WHERE  ID = OBJECT_ID('TMP_SUB_CATEGORIA')
@@ -58,15 +95,28 @@ IF EXISTS (SELECT 1
 go
 
 /*==============================================================*/
-/* Table: TB_CONTA                                              */
+/* Table: LOG_CAT_SUBCAT_NO_MATCH                               */
 /*==============================================================*/
-CREATE TABLE TB_CONTA (
-   CONTA_COD            INTEGER              IDENTITY(1,1),
-   CONTA_DESCRICAO      VARCHAR(50)          NOT NULL
-      CONSTRAINT CKC_CONTA_DESCRICAO_TB_CONTA CHECK (CONTA_DESCRICAO = UPPER(CONTA_DESCRICAO)),
-   CONTA_FLAG_ATIVO     BIT                  NOT NULL DEFAULT 1,
-   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE(),
-   CONSTRAINT PK_TB_CONTA PRIMARY KEY (CONTA_COD)
+CREATE TABLE LOG_CAT_SUBCAT_NO_MATCH (
+   NMC_CATEGORIA_DESC   VARCHAR(50)          NOT NULL,
+   NMC_CATEGORIA_NIVEL  SMALLINT             NOT NULL DEFAULT -1,
+   NMC_CATEGORIA_PAI_COD SMALLINT             NOT NULL DEFAULT -1,
+   NMC_FLAG_ATIVO       BIT                  NOT NULL DEFAULT 1,
+   NMC_DATA_CARGA       DATETIME             NOT NULL DEFAULT CURRENT_TIMESTAMP
+)
+go
+
+ALTER TABLE LOG_CAT_SUBCAT_NO_MATCH
+   ADD CONSTRAINT CKC_NMC_CATEGORIA_DES_LOG_CAT_ CHECK (NMC_CATEGORIA_DESC = UPPER(NMC_CATEGORIA_DESC))
+go
+
+/*==============================================================*/
+/* Table: LOG_CONTA_NO_MATCH                                    */
+/*==============================================================*/
+CREATE TABLE LOG_CONTA_NO_MATCH (
+   NMC_CONTA_DESC       VARCHAR(50)          NOT NULL,
+   NMC_CONTA_FLAG_ATIVO BIT                  NOT NULL DEFAULT 1,
+   NMC_DATA_CARGA       DATETIME             NOT NULL DEFAULT GETDATE()
 )
 go
 
@@ -74,19 +124,20 @@ DECLARE @CURRENTUSER SYSNAME
 SELECT @CURRENTUSER = USER_NAME()
 EXECUTE SP_ADDEXTENDEDPROPERTY 'MS_Description', 
    'Se a Conta estiver ativa, preencher como 1; senao 0',
-   'user', @CURRENTUSER, 'table', 'TB_CONTA', 'column', 'CONTA_FLAG_ATIVO'
+   'user', @CURRENTUSER, 'table', 'LOG_CONTA_NO_MATCH', 'column', 'NMC_CONTA_FLAG_ATIVO'
+go
+
+ALTER TABLE LOG_CONTA_NO_MATCH
+   ADD CONSTRAINT CKC_NMC_CONTA_DESC_LOG_CONT CHECK (NMC_CONTA_DESC = UPPER(NMC_CONTA_DESC))
 go
 
 /*==============================================================*/
-/* Table: TB_TRANSACAO                                          */
+/* Table: LOG_TRANSACAO_NO_MATCH                                */
 /*==============================================================*/
-CREATE TABLE TB_TRANSACAO (
-   TRANSACAO_COD        INTEGER              IDENTITY(1,1),
-   TRANSACAO_DESC       VARCHAR(50)          NOT NULL
-      CONSTRAINT CKC_TRANSACAO_DESC_TB_TRANS CHECK (TRANSACAO_DESC = UPPER(TRANSACAO_DESC)),
-   TRANSACAO_FLAG_ATIVO BIT                  NOT NULL DEFAULT 1,
-   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE(),
-   CONSTRAINT PK_TB_TRANSACAO PRIMARY KEY (TRANSACAO_COD)
+CREATE TABLE LOG_TRANSACAO_NO_MATCH (
+   TRANSACAO_DESC       VARCHAR(50)          NOT NULL,
+   FLAG_ATIVO           BIT                  NOT NULL DEFAULT 1,
+   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE()
 )
 go
 
@@ -94,7 +145,63 @@ DECLARE @CURRENTUSER SYSNAME
 SELECT @CURRENTUSER = USER_NAME()
 EXECUTE SP_ADDEXTENDEDPROPERTY 'MS_Description', 
    'Se a transacao estiver ativa, preencher como 1, senao 0',
-   'user', @CURRENTUSER, 'table', 'TB_TRANSACAO', 'column', 'TRANSACAO_FLAG_ATIVO'
+   'user', @CURRENTUSER, 'table', 'LOG_TRANSACAO_NO_MATCH', 'column', 'FLAG_ATIVO'
+go
+
+ALTER TABLE LOG_TRANSACAO_NO_MATCH
+   ADD CONSTRAINT CKC_TRANSACAO_DESC_LOG_TRAN CHECK (TRANSACAO_DESC = UPPER(TRANSACAO_DESC))
+go
+
+/*==============================================================*/
+/* Table: TB_CONTA                                              */
+/*==============================================================*/
+CREATE TABLE TB_CONTA (
+   CONTA_COD            INTEGER              IDENTITY(1,1),
+   CONTA_DESC           VARCHAR(50)          NOT NULL,
+   FLAG_ATIVO           BIT                  NOT NULL DEFAULT 1,
+   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE()
+)
+go
+
+DECLARE @CURRENTUSER SYSNAME
+SELECT @CURRENTUSER = USER_NAME()
+EXECUTE SP_ADDEXTENDEDPROPERTY 'MS_Description', 
+   'Se a Conta estiver ativa, preencher como 1; senao 0',
+   'user', @CURRENTUSER, 'table', 'TB_CONTA', 'column', 'FLAG_ATIVO'
+go
+
+ALTER TABLE TB_CONTA
+   ADD CONSTRAINT CKC_CONTA_DESC_TB_CONTA CHECK (CONTA_DESC = UPPER(CONTA_DESC))
+go
+
+ALTER TABLE TB_CONTA
+   ADD CONSTRAINT PK_TB_CONTA PRIMARY KEY (CONTA_COD)
+go
+
+/*==============================================================*/
+/* Table: TB_TRANSACAO                                          */
+/*==============================================================*/
+CREATE TABLE TB_TRANSACAO (
+   TRANSACAO_COD        INTEGER              IDENTITY(1,1),
+   TRANSACAO_DESC       VARCHAR(50)          NOT NULL,
+   FLAG_ATIVO           BIT                  NOT NULL DEFAULT 1,
+   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE()
+)
+go
+
+DECLARE @CURRENTUSER SYSNAME
+SELECT @CURRENTUSER = USER_NAME()
+EXECUTE SP_ADDEXTENDEDPROPERTY 'MS_Description', 
+   'Se a transacao estiver ativa, preencher como 1, senao 0',
+   'user', @CURRENTUSER, 'table', 'TB_TRANSACAO', 'column', 'FLAG_ATIVO'
+go
+
+ALTER TABLE TB_TRANSACAO
+   ADD CONSTRAINT CKC_TRANSACAO_DESC_TB_TRANS CHECK (TRANSACAO_DESC = UPPER(TRANSACAO_DESC))
+go
+
+ALTER TABLE TB_TRANSACAO
+   ADD CONSTRAINT PK_TB_TRANSACAO PRIMARY KEY (TRANSACAO_COD)
 go
 
 /*==============================================================*/
@@ -102,11 +209,9 @@ go
 /*==============================================================*/
 CREATE TABLE TMP_CATEGORIA (
    CATEGORIA_COD        INTEGER              NOT NULL,
-   CATEGORIA_DESC       VARCHAR(50)          NOT NULL
-      CONSTRAINT CKC_CATEGORIA_DESC_TMP_CATE CHECK (CATEGORIA_DESC = UPPER(CATEGORIA_DESC)),
-   CATEGORIA_FLAG_ATIVO BIT                  NOT NULL DEFAULT 1,
-   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE(),
-   CONSTRAINT PK_TMP_CATEGORIA PRIMARY KEY (CATEGORIA_COD)
+   CATEGORIA_DESC       VARCHAR(50)          NOT NULL,
+   FLAG_ATIVO           BIT                  NOT NULL DEFAULT 1,
+   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE()
 )
 go
 
@@ -114,7 +219,15 @@ DECLARE @CURRENTUSER SYSNAME
 SELECT @CURRENTUSER = USER_NAME()
 EXECUTE SP_ADDEXTENDEDPROPERTY 'MS_Description', 
    'Se a categoria estiver ativa, preencher como 1; senao 0',
-   'user', @CURRENTUSER, 'table', 'TMP_CATEGORIA', 'column', 'CATEGORIA_FLAG_ATIVO'
+   'user', @CURRENTUSER, 'table', 'TMP_CATEGORIA', 'column', 'FLAG_ATIVO'
+go
+
+ALTER TABLE TMP_CATEGORIA
+   ADD CONSTRAINT CKC_CATEGORIA_DESC_TMP_CATE CHECK (CATEGORIA_DESC = UPPER(CATEGORIA_DESC))
+go
+
+ALTER TABLE TMP_CATEGORIA
+   ADD CONSTRAINT PK_TMP_CATEGORIA PRIMARY KEY (CATEGORIA_COD)
 go
 
 /*==============================================================*/
@@ -149,11 +262,9 @@ go
 /*==============================================================*/
 CREATE TABLE TMP_SUB_CATEGORIA (
    SUB_CATEGORIA_COD    INTEGER              NOT NULL,
-   SUB_CATEGORIA_DESC   VARCHAR(50)          NOT NULL
-      CONSTRAINT CKC_SUB_CATEGORIA_DES_TMP_SUB_ CHECK (SUB_CATEGORIA_DESC = UPPER(SUB_CATEGORIA_DESC)),
-   SUB_CATEGORIA_FLAG_ATIVO BIT                  NOT NULL DEFAULT 1,
-   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE(),
-   CONSTRAINT PK_TMP_SUB_CATEGORIA PRIMARY KEY (SUB_CATEGORIA_COD)
+   SUB_CATEGORIA_DESC   VARCHAR(50)          NOT NULL,
+   FLAG_ATIVO           BIT                  NOT NULL DEFAULT 1,
+   DATA_CARGA           DATETIME             NOT NULL DEFAULT GETDATE()
 )
 go
 
@@ -161,7 +272,15 @@ DECLARE @CURRENTUSER SYSNAME
 SELECT @CURRENTUSER = USER_NAME()
 EXECUTE SP_ADDEXTENDEDPROPERTY 'MS_Description', 
    'Se a categoria estiver ativa, preencher como 1; senao 0',
-   'user', @CURRENTUSER, 'table', 'TMP_SUB_CATEGORIA', 'column', 'SUB_CATEGORIA_FLAG_ATIVO'
+   'user', @CURRENTUSER, 'table', 'TMP_SUB_CATEGORIA', 'column', 'FLAG_ATIVO'
+go
+
+ALTER TABLE TMP_SUB_CATEGORIA
+   ADD CONSTRAINT CKC_SUB_CATEGORIA_DES_TMP_SUB_ CHECK (SUB_CATEGORIA_DESC = UPPER(SUB_CATEGORIA_DESC))
+go
+
+ALTER TABLE TMP_SUB_CATEGORIA
+   ADD CONSTRAINT PK_TMP_SUB_CATEGORIA PRIMARY KEY (SUB_CATEGORIA_COD)
 go
 
 /*==============================================================*/
@@ -170,8 +289,8 @@ go
 CREATE VIEW TMP_CONTA_DESTINO AS
 SELECT
    CONTA_COD AS CONTA_DESTINO_COD,
-   CONTA_DESCRICAO,
-   CONTA_FLAG_ATIVO,
+   CONTA_DESC,
+   FLAG_ATIVO,
    DATA_CARGA
 FROM
    TB_CONTA
@@ -191,6 +310,6 @@ SELECT
 FROM
    TB_CATEG_SUBCATEG
 WHERE
-   CATEGORIA_NIVEL IS NULL
+   CATEGORIA_NIVEL = '-1'
 go
 
